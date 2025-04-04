982e20c3bfb8cf4c4eeaf51cfadca84cdfa7a3e8
Matthew Camacho <mcamacho2056@gmail.com>
2025-04-04 11:23:09 -0600

    allowed the script to loop through cameras when the space bar is pressed

--- FnafOneRecreationAttempt3/Assets/Scripts/CameraSwitchScripts.cs
+++ FnafOneRecreationAttempt3/Assets/Scripts/CameraSwitchScripts.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class CameraSwitchScripts : MonoBehaviour
 {
     public Camera[] cameraList;
+
+    [SerializeField] private int waitLimit = 100;
+
     private bool isCycling = false;
+
+    private int waitTime = 0;
+    private int cameraIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var camera in cameraList)
+        {
+            camera.enabled = false;
+        }
+        cameraList[0].enabled = true;
     }
 
     // Update is called once per frame
@@ -17,17 +27,35 @@ public class CameraSwitchScripts : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            
             isCycling = !isCycling;
         }
         if (isCycling)
         {
-            CycleThroughCameras();
+            waitTime++;
+            if (waitTime >= waitLimit)
+            {
+                waitTime = 0;
+                CycleThroughCameras();
+            }
+            
         }
     }
 
     private void CycleThroughCameras()
     {
+        cameraIndex += 1;
+        if (cameraIndex >= cameraList.Length) 
+        {
+            
+            cameraIndex = 0;
+        }
 
+        foreach (var camera in cameraList)
+        {
+            camera.enabled = false;
+        }
+        cameraList[cameraIndex].enabled = true;
     }
-    
+
 }
